ec15c6b9cb357eefbae271b50992ac43d6538a9d
Omarciano19 <omarciano19@gmail.com>
2024-03-05 15:37:14 -0600

    Multiples graficas, checkbokes, boton para mostrar el dataframe estilizado.

--- app.py
+++ app.py
@@ -5,11 +5,22 @@ import pandas as pd
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+st.header('Despliegue web de un análisis estadístico simple de anuncios de coches.')
+
+show_df = st.button('Mostrar DataFrame')
+
+hist_check = st.checkbox('Construir histograma')  # crear un botón
+
 disp_check = st.checkbox(
     'Construir grafico de dispersión')  # crear un checkbox
 
-if hist_button:  # al hacer clic en el botón
+boxplot_check = st.checkbox(
+    'Construir grafico de caja de precios')  # crear un checkbox
+
+color_check = st.checkbox(
+    'Averigua que colores son mas raros con este grafico de barras.')  # crear un checkbox
+
+if hist_check:  # al hacer clic en el botón
     # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -23,10 +34,40 @@ if hist_button:  # al hacer clic en el botón
 if disp_check:  # al verificar la checkbox
     # escribir un mensaje
     st.write(
-        'Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
+        'Creación de un grafico de dispersión comparando precios y año del modelo.')
 
     # crear un histograma
     fig2 = px.scatter(car_data, y="price", x="model_year")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig2, use_container_width=True)
+
+
+if boxplot_check:  # al verificar la checkbox
+    # escribir un mensaje
+    st.write(
+        'Creación de un grafico de caja para los precios de venta de coches')
+
+    # crear un histograma
+    fig = px.box(car_data, x="type", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+if color_check:  # al verificar la checkbox
+    # escribir un mensaje
+    st.write('Creación de un histograma de los colores de autos:')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="paint_color")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if show_df:
+    # Aplicar estilos al dataframe
+    car_data_styled = car_data.style.set_properties(**{'background-color': 'black',
+                                                       'color': 'lawngreen',
+                                                       'border-color': 'white'})
+    st.write(car_data_styled)
