36fbfc07e0186337f88ab30be1f28564b78a9fb8
Omarciano19 <omarciano19@gmail.com>
2024-03-05 15:22:26 -0600

    Primer checkbox

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
+disp_check = st.checkbox(
+    'Construir grafico de dispersión')  # crear un checkbox
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -17,15 +19,14 @@ if hist_button:  # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-disp_button = st.button('Construir grafico de dispersión')  # crear un botón
 
-if disp_button:  # al hacer clic en el botón
+if disp_check:  # al verificar la checkbox
     # escribir un mensaje
     st.write(
         'Creación de un grafico de dispersión para el conjunto de datos de anuncios de venta de coches')
 
     # crear un histograma
-    fig2 = px.scatter(car_data, y="price", x="odometer")
+    fig2 = px.scatter(car_data, y="price", x="model_year")
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig2, use_container_width=True)
